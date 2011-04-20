63060b24426f701a0ee40fcffcfe871948196fda
k_l_ <k_l_@2774ca72-3a49-0410-b1b8-fd8ace3c0660>
2011-04-20 01:31:07 +0000

    WindowPoser: added parameter 'class'.

--- WindowPoser/main.cpp
+++ WindowPoser/main.cpp
@@ -13,6 +13,7 @@ int main(int argc, char* argv[])
 	po::options_description desc("Allowed options");
 	desc.add_options()
 		("hwnd",		po::value<unsigned long>())
+		("class",		po::value<std::string>())
 		("title",		po::value<std::string>())
 		("x",			po::value<int>())
 		("y",			po::value<int>())
@@ -24,18 +25,19 @@ int main(int argc, char* argv[])
 	po::store(po::parse_command_line(argc, argv, desc), vm);
 	po::notify(vm);
 
-	::HWND hwnd = vm.count("hwnd") ? ::HWND(vm["hwnd"].as<unsigned long>()) : ::FindWindow(NULL, vm["title"].as<std::string>().data());
+	::HWND hwnd = vm.count("hwnd") ? ::HWND(vm["hwnd"].as<unsigned long>()) : ::FindWindow(vm.count("class") ? vm["class"].as<std::string>().data() : NULL, vm.count("title") ? vm["title"].as<std::string>().data() : NULL);
 	if(hwnd)
 	{
-		const int x = vm.count("x") ? vm["x"].as<int>() : 0;
-		const int y = vm.count("y") ? vm["y"].as<int>() : 0;
-		const int width = vm.count("width") ? vm["width"].as<int>() : 171;
-		const int height = vm.count("height") ? vm["height"].as<int>() : 237;
+		const int x = vm.count("x") ? vm["x"].as<int>() : -1;
+		const int y = vm.count("y") ? vm["y"].as<int>() : -1;
+		const int width = vm.count("width") ? vm["width"].as<int>() : -1;
+		const int height = vm.count("height") ? vm["height"].as<int>() : -1;
 
 		::SetForegroundWindow(hwnd);
 		long style = ::GetWindowLong(hwnd, GWL_STYLE);
 		int succeed = ::SetWindowLong(hwnd, GWL_STYLE, style | WS_EX_TOPMOST);
-		succeed = ::SetWindowPos(hwnd, HWND_TOPMOST, x, y, width, height, SWP_SHOWWINDOW);
+		if(x >= 0 && y >= 0 && width >= 0 && height>= 0)
+			succeed = ::SetWindowPos(hwnd, HWND_TOPMOST, x, y, width, height, SWP_SHOWWINDOW);
 		::SetForegroundWindow(hwnd);
 
 		return 0;
